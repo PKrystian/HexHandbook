a4efee06c5a0bb2ae2cf2517015a9155dd58dd2f
PKrystian <krystian.pinczak2001@gmail.com>
2024-01-29 22:25:24 +0100

    [HEX] App.js test fix

--- App.js
+++ App.js
@@ -1,53 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ScrollView } from 'react-native';
-import { Table, Row } from 'react-native-table-component';
-import { Divider } from 'react-native-elements';
+import React, { useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View, ScrollView } from 'react-native'
+import { Table, Row } from 'react-native-table-component'
+import { Divider } from 'react-native-elements'
 
-export default function App() {
-    const [data, setData] = useState([]);
+export default function App () {
+  const [data, setData] = useState([])
 
-    useEffect(() => {
-        fetchSpells();
-    }, []);
+  useEffect(() => {
+    fetchSpells()
+  }, [])
 
-    const fetchSpells = async () => {
-        try {
-            const response = await fetch('./spells.json');
-            const json = await response.json();
-            setData(json);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const fetchSpells = async () => {
+    try {
+      const response = await fetch('./spells.json')
+      const json = await response.json()
+      setData(json)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-    const tableHead = ['Name', 'Description', 'Level', 'Range', 'Components', 'Duration', 'Casting Time', 'Ritual', 'School', 'Type', 'Tags', 'Classes', 'Higher Levels'];
+  const tableHead = ['Name', 'Description', 'Level', 'Range', 'Components', 'Duration', 'Casting Time', 'Ritual', 'School', 'Type', 'Tags', 'Classes', 'Higher Levels']
 
-    return (
+  return (
         <ScrollView style={styles.container}>
             {data.map((spell, index) => {
-                const components = Object.entries(spell.components)
-                    .map(([key, value]) => `${key}: ${value}`)
-                    .join(', ');
+              const components = Object.entries(spell.components)
+                .map(([key, value]) => `${key}: ${value}`)
+                .join(', ')
 
-                const spellData = { ...spell, components };
+              const spellData = { ...spell, components }
 
-                return (
-                    <View key={index}>
-                        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-                            <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
-                            <Row data={Object.values(spellData)} textStyle={styles.text}/>
-                        </Table>
-                        <Divider style={{ backgroundColor: 'blue' }} />
-                    </View>
-                );
+              return (
+                <View key={index}>
+                    <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                        <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+                        <Row data={Object.values(spellData)} textStyle={styles.text} />
+                    </Table>
+                    <Divider style={{ backgroundColor: 'blue' }} />
+                </View>
+              )
             })}
             <StatusBar style="auto" />
         </ScrollView>
-    );
+  )
 }
 
 const styles = StyleSheet.create({
-    head: { height: 40, backgroundColor: '#f1f8ff' },
-    text: { margin: 6 },
-});
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
+})
