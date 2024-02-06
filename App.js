81d0cf54e346930f24ab55667d972e0ebb9f263d
PKrystian <krystian.pinczak2001@gmail.com>
2024-02-06 16:57:33 +0100

    [HEX] Expo update to 50 + other small upgrades + responsive react app fix

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import SpellList from './SpellList'
 
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#393E46' }}>
-      <SpellList />
-    </SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#393E46' }}>
+        <SpellList />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   )
 }
 
